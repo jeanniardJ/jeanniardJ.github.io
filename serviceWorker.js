d4681423e937a60609fa3d8332e1533a585aff30
Jonasbadboys <jonathanjeanniard@sfr.fr>
2021-03-19 19:46:23 +0100

    test du service

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,13 @@
 const staticCacheName = "cache-v1";
-const assets = ['/', 'index.htmlgi']
+const assets = ['/', 'index.html']
 self.addEventListener('install', (e)=>{
     e.waitUntil(
         caches.open(staticCacheName).then((cache) => {
-            cache.addAll(assets)
+            cache.addAll(assets);
         })
     )
+})
+
+self.addEventListener('fetch', (e) =>{
+    console.log(e)
 })
