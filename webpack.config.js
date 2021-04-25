7c392d1fc1310150d246b0dda3d30d6bcfa7dbf1
Jonasbadboys <jonathanjeanniard@sfr.fr>
2021-04-25 22:14:08 +0200

    prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
-const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const dev = process.env.NODE_ENV === "dev";
 
-module.exports = {
+let config = {
+    mode: dev ? "development" : "production",
     entry: "./src/app.js",
+    watch: dev,
     output: {
         filename: "app.js",
         path: path.resolve(__dirname, "./assets")
     },
+    devtool: dev ? "eval-cheap-module-source-map" : false,
     module: {
         rules: [
             {
@@ -26,29 +30,26 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             }, {
                 test: /\.scss$/,
-                loader: 'postcss-loader',    
+                loader: 'postcss-loader',
                 options: {
                     // `postcssOptions` is needed for postcss 8.x;
                     // if you use postcss 7.x skip the key
                     postcssOptions: {
-                      // postcss plugins, can be exported to postcss.config.js
-                      plugins: function () {
-                        return [
-                          require('autoprefixer')
-                        ];
-                      }
+                        // postcss plugins, can be exported to postcss.config.js
+                        plugins: function () {
+                            return [
+                                require('autoprefixer')
+                            ];
+                        }
                     }
-                  }
+                }
             }, {
-                rules: [
-                    {
-                        test: /\.css$/,
-                        use: [
-                            'style-loader',
-                            'css-loader'
-                        ]
-                    }
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
                 ]
+
             }, {
                 test: /\.(png|jpg|gif)$/,
                 use: [
@@ -64,5 +65,12 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: []
 };
+
+if(!dev){
+    config.plugins.push(new UglifyJsPlugin())
+}
+
+module.exports = config
