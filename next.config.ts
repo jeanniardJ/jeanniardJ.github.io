ad8a4e44a4cb2a584ac812131fd066ff595a2d3c
Jonathan Jeanniard <jonathanjeanniard@sfr.fr>
2025-12-19 16:37:14 +0000

    Build portfolio et mise ne prod

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/jeanniardJ.github.io",
+  reactStrictMode: true,
+  distDir: "build",
   images: {
     unoptimized: true,
-  }
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
